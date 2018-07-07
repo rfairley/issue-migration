2bfddd4630d27dec583f5040c1872578552a227f
Robert Fairley <rfairley@users.noreply.github.com>
2018-07-06 22:17:35 -0400

    Typo fixes, reformatting, add comment detail

--- README.md
+++ README.md
@@ -5,7 +5,17 @@ Then, just run the script from within this repo directory.
 
 ```
 cd issue-migration
+chmod +x ./move.sh
 ./move.sh
 ```
 
 On success, messages will be printed to the console as each issue is processed one by one. Takes about 2 seconds per issue.
+
+The script migrates:
+- issue title
+- issue body (reposted)
+- issue comments (reposted)
+- one assignee only, not multiple
+- labels attached to the migrated issues
+
+See https://github.com/rfairley/migration-test-finish/issues/12 for an example of a migrated issue.
--- move.sh
+++ move.sh
@@ -8,28 +8,38 @@ set -e
 # Instructions:
 # 1) make sure this script is being run in a new directory (running it in the repo is good).
 # 2) set the .*_OWNER and .*_REPO variables (see indicated below)
-# 3) set the AUTHORIZATION_TOKEN to access the GitHub API. A Personal Access Token form GitHub will allow a rate limit of 5000
+# 3) set the AUTHORIZATION_TOKEN to access the GitHub API. A Personal Access Token from GitHub will allow a rate limit of 5000
 #    requests per hour, which should be enough for the migration.
 # 4) MIGRATE_LABEL - set to the label indicating which issues to move (see indicated below)
 # 5) DEST_LABEL - set to a label that corresponds with your DESTINATION_REPO (see indicated below)
 
-# In summary, only issues that have both MIGRATE_LABEL AND DEST_LABEL will be migrated.
-# In my example in migration-test-start, these labels are "component/test" and "kind/otherlabel".
-# To test for certain that this works the way it is intended, the POST and PATCH requests can be
-# commented out (see comments near end of file). This will be a dry run that doesn't affect the
-# SOURCE_REPO - only the DESTINATION_REPO. 
+# Migration criteria:
+# Only issues that have both MIGRATE_LABEL AND DEST_LABEL will be migrated.
+# Pull requests will not be migrated.
+# In my example in https://github.com/rfairley/migration-test-start, these labels are
+# "component/test" and "kind/otherlabel" respectively (see the now closed issues).
 
-SOURCE_OWNER= # <-- coreos
-DESTINATION_OWNER= # <-- coreos
-SOURCE_REPO=migration-test-start # <-- bugs
-DESTINATION_REPO=migration-test-finish # <-- ignition or coreos-metadata
+# Testing/Dry run:
+# To test that this works the way it is intended, the POST and PATCH
+# requests can be commented out (see two comments near end of this file). This will
+# be a dry run that doesn't affect the SOURCE_REPO - only the DESTINATION_REPO.
 
-AUTHORIZATION_TOKEN= # Comments and issues are published by whichever acccount owns this token.
+SOURCE_OWNER=rfairley 			# <-- coreos
+DESTINATION_OWNER=rfairley		# <-- coreos
+SOURCE_REPO=migration-test-start 	# <-- bugs
+DESTINATION_REPO=migration-test-finish 	# <-- ignition or coreos-metadata
 
-MIGRATE_LABEL="migrate/me" # <-- "needs/migration"
-DEST_LABEL="desination/indicator" # <-- "component/ignition" or "component/coreos-metadata"
+AUTHORIZATION_TOKEN= 			# Comments and issues are published by whichever acccount owns this token.
+					# Should be a token associated with coreosbot.
 
-ISSUE_NUMBERS_TO_MIGRATE=issue_numbers.txt # Doesn't matter the name of this, it gets created and deleted while script runs.
+MIGRATE_LABEL="component/test" 		# <-- "needs/migration"
+DEST_LABEL="kind/otherlabel" 		# <-- "component/ignition" or "component/coreos-metadata"
+
+ISSUE_NUMBERS_TO_MIGRATE=issue_numbers.txt # Doesn't matter the name of this file, it gets created and deleted while script runs.
+
+echo "Note: if errors are given, make sure environment variables are set!"
+echo "Note: if no issues to be moved are found, an error will result."
+
 
 escape() {
 	sed \
@@ -48,8 +58,6 @@ raw_issues=$(curl \
 	--header "Accept: application/vnd.github.golden-comet-preview+json" \
 	--url "https://api.github.com/repos/${SOURCE_OWNER}/${SOURCE_REPO}/issues")
 
-echo $raw_issues > raw
-
 [ -e $ISSUE_NUMBERS_TO_MIGRATE ] && rm $ISSUE_NUMBERS_TO_MIGRATE
 
 issue_count=$(jq "length" --raw-output <<< ${raw_issues})
@@ -58,28 +66,26 @@ echo "Found ${issue_count} issues total"
 
 # Generate a list of issues to migrate based on matching label.
 for i in $(seq 0 $((${issue_count} - 1))); do
+	# If the issue is a pull request, then don't migrate it.
+	if grep --invert-match null <<< $(jq ".[$i].pull_request" <<< ${raw_issues}) > /dev/null; then
+		continue
+	fi
+	
 	issue_labels=$(jq ".[$i].labels" --raw-output <<< ${raw_issues})
 	labels_length=$(jq "length" --raw-output <<< ${issue_labels})
 	has_migrate_label="false"
 	has_dest_label="false"
 	for j in $(seq 0 $((${labels_length} - 1))); do
-		# If the issue is a pull request, then don't migrate it.
-		if grep --invert-match null <<< $(jq ".[${i}].pull_request" <<< ${raw_issues}) > /dev/null; then
-			continue
-		fi
-
 		# Check for having the migrate indicator label and the destination label.
 		if [ "$(jq ".[$j].name" --raw-output <<< ${issue_labels})" == "${MIGRATE_LABEL}" ]; then
 			has_migrate_label="true"
 		fi
-
 		if [ "$(jq ".[$j].name" --raw-output <<< ${issue_labels})" == "${DEST_LABEL}" ]; then
 			has_dest_label="true"
 		fi
 	done
 
 	if [ $has_migrate_label = "true" ] && [ $has_dest_label = "true" ]; then
-		echo "here"
 		echo "$(jq ".[$i].number" --raw-output <<< ${raw_issues})" >> issue_numbers_raw
 	fi
 done
@@ -90,7 +96,7 @@ tac issue_numbers_raw > $ISSUE_NUMBERS_TO_MIGRATE
 echo "Migrating the following issue numbers"
 cat $ISSUE_NUMBERS_TO_MIGRATE
 
-## ---- Below this can be a separate script
+## ---- Migration begins now
 
 echo "Starting migration..."
 
@@ -121,11 +127,8 @@ while read issue_number; do
 		--url $(jq ".comments_url" --raw-output <<< ${raw_issue})
 	)
 
-	#echo $raw_comments
-
 	comment_count=$(jq "length" --raw-output <<< ${raw_comments})
 
-	echo $comment_count
 	comments="[]"
 	for i in $(seq 0 $((${comment_count} - 1))); do
 		comment_body=$(cat <<-EOF
